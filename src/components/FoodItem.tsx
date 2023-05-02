046995cafed76219d6a0ea0d1898f5a48e46b61e
cmmille <cmmiller.bio@gmail.com>
2023-05-02 16:59:19 -0700

    Add animation

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import { Food } from "../models/Food";
 
 interface Props {
@@ -8,12 +10,24 @@ const FoodItem = ({ food }: Props) => {
   const { name, image } = food ? food : { name: "", image: "" };
 
   return (
-    <div className="w-full max-w-xs relative">
+    <motion.div
+      animate={{
+        scale: [0, 1],
+        rotate: [0, 270, 0],
+        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+      }}
+      transition={{
+        duration: 1,
+        delay: 0.25,
+      }}
+      className="w-full max-w-xs relative"
+    >
+      {food && <h1 className="text-4xl font-cute">Ryan should cook:</h1>}
       <h2 className="text-red-600 -translate-x-1/2 font-bold absolute bottom-2 left-1/2 text-shadow text-4xl font-cute">
         {name}
       </h2>
       <img src={image} alt={name} />
-    </div>
+    </motion.div>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,16 +5,18 @@ import FoodItem from "../components/FoodItem";
 
 const Home = () => {
   const [selectedFood, setSelectedFood] = useState<Food | null>(null);
+  const [count, setCount] = useState(0);
 
   function randomFood() {
     const index = Math.floor(Math.random() * FoodList.length);
     const food = FoodList[index];
     setSelectedFood(food);
+    setCount(count + 1);
   }
 
   return (
-    <div className="w-full text-center flex flex-col justify-center items-center">
-      <h1 className="text-8xl mb-4 font-cute">Help Ryan Cook!</h1>
+    <div className="w-full text-center flex flex-col justify-center items-center gap-2">
+      <h1 className="text-8xl mb-4 font-cute">What should Ryan Cook?!</h1>
       <button
         className="bg-lime-900 p-4 text-5xl rounded-md hover:bg-green-900 font-cute"
         onClick={randomFood}
@@ -22,7 +24,7 @@ const Home = () => {
         Help Ryan!
       </button>
 
-      <FoodItem food={selectedFood} />
+      <FoodItem food={selectedFood} key={count} />
     </div>
   );
 };
