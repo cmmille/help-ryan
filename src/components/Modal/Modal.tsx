f0fc2147f60b818e20153ab714888c8f146083ac
cmmille <cmmiller.bio@gmail.com>
2023-05-03 10:37:02 -0700

    Animate modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import { createPortal } from "react-dom";
 import Backdrop from "./Backdrop";
+import { AnimatePresence, Variants, motion } from "framer-motion";
 
 interface ModalProps {
   isOpen: boolean;
@@ -7,20 +8,48 @@ interface ModalProps {
   children?: React.ReactNode;
 }
 
+const animationVariants: Variants = {
+  hidden: {
+    y: "-100vh",
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      type: "spring",
+      damping: 20,
+      stiffness: 250,
+      duration: 0.1,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
+
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   return createPortal(
-    <>
+    <AnimatePresence initial={false}>
       {isOpen && (
         <Backdrop onClose={onClose}>
-          <div
+          <motion.div
             className="bg-zinc-900 rounded-lg shadow-lg p-8"
             onClick={(e) => e.stopPropagation()}
+            variants={animationVariants}
+            key={"modal"}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
           >
             {children}
-          </div>
+          </motion.div>
         </Backdrop>
       )}
-    </>,
+    </AnimatePresence>,
     document.getElementById("modal-root") as HTMLElement
   );
 };
